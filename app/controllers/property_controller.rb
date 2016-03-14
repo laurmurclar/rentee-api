38c2a202a076af9ec0181890351860b337255060
Brian McNestry <brianmcnestry@gmail.com>
2016-03-14 21:54:11 +0000

    started on creating a property

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -6,4 +6,17 @@ class PropertyController < ActionController::API
     render json: @property
   end
 
+  def create
+  	respond_with Property.create(
+  		landlord_id: params[:landlord_id],
+  		street: params[:street],
+  		town: params[:town],
+  		county: params[:county],
+  		rent: params[:rent],
+  		n_baths: params[:n_baths],
+  		rent_allowance: params[:rent_allowance],
+  		ptrb: params[:ptrb]
+  	)
+  end
+
 end
