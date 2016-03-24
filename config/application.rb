b703d0ef0c0a1d947d72df2d8f5b94ca8656fb9e
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-03-24 12:22:09 +0000

    add options method to cross

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module RenteeApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
       end
     end
 
