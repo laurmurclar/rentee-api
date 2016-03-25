5a0227797bae2766ccd58e7577e754e549afac41
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-03-25 15:06:19 +0000

    updated data in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,38 @@
 properties = Property.create([
   {
     landlord_id: 1,
-    street: 'Apt 4, Oak Avenue',
-    town: 'Baloo',
-    county: 'Louth',
+    street: 'Red Cottage',
+    town: 'Little Town',
+    county: 'Sligo',
+    rent: 200,
+    n_baths: 1,
+    rent_allowance: false,
+    ptrb: true,
+    n_beds: 3,
+    avail_beds: 3
+  },
+  {
+    landlord_id: 2,
+    street: 'Apt 6, Oak Avenue',
+    town: 'Balooth',
+    county: 'Leitrim',
     rent: 500,
     n_baths: 2,
     rent_allowance: true,
-    ptrb: true
+    ptrb: true,
+    n_beds: 5,
+    avail_beds: 3
+  },
+  {
+    landlord_id: 2,
+    street: 'Apt 72, Windy Walk',
+    town: 'Balooth',
+    county: 'Leitrim',
+    rent: 550,
+    n_baths: 2,
+    rent_allowance: true,
+    ptrb: true,
+    n_beds: 5,
+    avail_beds: 3
   }
 ])
