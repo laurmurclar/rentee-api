afb59f655cf73dffebca783d0bcae26a6ff2fb16
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-03-25 13:21:03 +0000

    fixed typo in amenties model

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -2,5 +2,5 @@ class Amenity < ActiveRecord::Base
 	belongs_to :property
 
 	validates :name, presence: true, length: { minimum: 5, maximum: 20 }
-	validates :property_id. presence: true
+	validates :property_id, presence: true
 end
