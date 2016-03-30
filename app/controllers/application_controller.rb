cd3cc8cdd69413478def3aa251b8e567c40877ff
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-03-30 14:23:30 +0100

    add devise_toke_auth_group for tenants & landlords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  devise_token_auth_group :user, contains: [:tenant, :landlord]
+  
   def root
     render json: { api: "v1" }
   end
